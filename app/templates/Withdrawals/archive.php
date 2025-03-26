43a5b3cd35c708eccb9da799818a5cf36aa4ee60
Andrii Vytovtov <andrey.vitovtov@gmail.com>
2025-03-26 15:30:53 +0200

    updated withdrawals archive

--- app/templates/Withdrawals/archive.php
+++ app/templates/Withdrawals/archive.php
@@ -18,34 +18,40 @@
         <th><?= __('balance') ?></th>
         <th><?= __('wallet') ?></th>
         <th><?= __('status') ?></th>
-        <th class="table-actions"><?= __('actions') ?></th>
+        <th>
+            <div class="table-actions">
+				<?= __('actions') ?>
+            </div>
+        </th>
     </tr>
     </thead>
     <tbody>
 	<?php foreach ($withdrawals ?? [] as $withdrawal):
 		$username = $withdrawal->user->username;
 		if (empty($username)) $username = $withdrawal->user->first_name . ' ' . $withdrawal->user->last_name;
-	?>
+		?>
 
         <tr>
             <td><?= $username ?></td>
             <td><?= $withdrawal->balance ?></td>
             <td><?= $withdrawal->wallet ?></td>
             <td><?= __($withdrawal->status) ?></td>
-            <td class="table-actions">
-                <button form="delete-withdrawals-<?= $withdrawal->id ?>" class="btn">
-                    <i class="icon-trash"></i>
-                    <span class="desk"><?= __('delete') ?></span>
-                </button>
-                <form action="/withdrawals/delete" method="POST"
-                      class="form-confirm"
-                      id="delete-withdrawals-<?= $withdrawal->id ?>"
-                      data-title="<?= __('deletion confirmation') ?>"
-                      data-body="<?= __('are you sure you want to remove the withdrawal') ?>"
-                      data-id="delete-withdrawals-<?= $withdrawal->id ?>"
-                >
-                    <input type="hidden" name="id" value="<?= $withdrawal->id ?>">
-                </form>
+            <td>
+                <div class="table-actions">
+                    <button form="delete-withdrawals-<?= $withdrawal->id ?>" class="btn">
+                        <i class="icon-trash"></i>
+                        <span class="desk"><?= __('delete') ?></span>
+                    </button>
+                    <form action="/withdrawals/delete" method="POST"
+                          class="form-confirm"
+                          id="delete-withdrawals-<?= $withdrawal->id ?>"
+                          data-title="<?= __('deletion confirmation') ?>"
+                          data-body="<?= __('are you sure you want to remove the withdrawal') ?>"
+                          data-id="delete-withdrawals-<?= $withdrawal->id ?>"
+                    >
+                        <input type="hidden" name="id" value="<?= $withdrawal->id ?>">
+                    </form>
+                </div>
             </td>
         </tr>
 	<?php endforeach; ?>
